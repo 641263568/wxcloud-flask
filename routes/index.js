a8b28b3614e29d20cb6e5927e61093bda93c0373
songchao <songchao22@countrygarden.com.cn>
2023-05-11 16:27:49 +0800

    init

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,45 @@
-const express = require('express')
-const wxapi = require('../work/wxapi')
-const router = express.Router()
+const express = require("express");
+const wxapi = require("../work/wxapi");
+const router = express.Router();
 
-router.get('/', async function (req, res, next) {
-  res.render('index', {})
-})
+const articles = [
+  {
+    title,
+    author: "远程程序员",
+    content,
+    thumb_media_id: media_id,
+  },
+];
+async function createDraft() {
+  const url = `http://api.weixin.qq.com/cgi-bin/draft/add`;
+  try {
+    await wxapi.call(url, {
+      articles,
+    });
+    console.log("消息推送成功");
+  } catch (error) {
+    console.error("消息推送失败:", error);
+    throw error;
+  }
+}
 
-router.post('/sec', async function (req, res, next) {
+router.get("/", async function (req, res, next) {
+  createDraft();
+  res.send("success");
+});
+
+router.post("/sec", async function (req, res, next) {
   let result = {
     errcode: 0,
-    errmsg: 'ok'
-  }
-  const text = req.body.content || null
+    errmsg: "ok",
+  };
+  const text = req.body.content || null;
   if (text != null) {
-    result = await wxapi.call('wxa/msg_sec_check', {
-      content: text
-    })
+    result = await wxapi.call("wxa/msg_sec_check", {
+      content: text,
+    });
   }
-  res.json(result)
-})
+  res.json(result);
+});
 
-module.exports = router
+module.exports = router;
