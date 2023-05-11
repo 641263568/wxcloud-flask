f4dde776ce885123d4db4459354b1784650bfc9f
songchao <songchao22@countrygarden.com.cn>
2023-05-11 14:52:34 +0800

    try createdraft

--- utils/createDraft.js
+++ utils/createDraft.js
@@ -1,13 +1,13 @@
 const axios = require("axios");
 async function createDraft(articles) {
   const url = `http://api.weixin.qq.com/cgi-bin/draft/add`;
-  const response = await axios.post(url, {
-    articles,
-  });
-  if (response.data.errcode) {
-    console.error(`Failed to create draft: ${response.data.errmsg}`);
-  } else {
-    console.log(`草稿推送成功: ${response.data.media_id}`);
+  try {
+    await axios.post(url, {
+      articles,
+    });
+  } catch (error) {
+    console.error("Error in createDraft():", error);
+    throw error;
   }
 }
 
