121d293e4652133dbb1303c336ccb240cc0e5fda
songchao <songchao22@countrygarden.com.cn>
2023-05-23 17:59:04 +0800

    add gpt

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const xmlparser = require("express-xml-bodyparser");
+const dotenv = require("dotenv");
+
 const app = express();
 
 app.use(xmlparser());
 
 const router = require("./router");
-
 app.use("/", router);
+
+dotenv.config(); // 读取并加载 .env 文件中的环境变量
 
 app.listen(3008, function () {
   console.log("服务启动成功！");
