dfd8814a1eb4161c41699236fc92b0604fc7af27
songchao <songchao22@countrygarden.com.cn>
2023-05-23 17:33:44 +0800

    add gpt

--- controllers/handleGptMessage.js
+++ controllers/handleGptMessage.js
@@ -1,7 +1,11 @@
 // 处理GPT消息回调
 const handleGptMessage = async (req, res) => {
-  // 为什么要先响应：微信服务器会在5秒内收到响应，才会认为你已经收到了消息，不会再次发送
-  res.send("你好");
+  const data = {
+    message: "Hello, World!",
+    count: 42,
+  };
+  res.json(data);
+  //   res.send("你好");
 };
 
 module.exports = {
